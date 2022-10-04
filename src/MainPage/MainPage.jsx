d8fdf5b74125e1aa802f5609407e25050a36abf0
limdumb <dlaruddls@nate.com>
2022-10-04 11:38:56 +0900

    Wirefram 화면 비율설정

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -7,34 +7,54 @@ const MainHeader = styled.header `
 
 const ChartSection = styled.section `
   display: flex;
-  justify-content: center;
   border: 1px solid black;
-  align-items: center;
   flex-direction: column;
   border-left: none;
   border-right: none;
   margin-top: 10px;
+  border-top: none;
+  height: 80%;
+`
+
+const Information = styled.section `
+  display: flex;
+  border: 1px solid black;
+  flex-direction: column;
+  border-left: none;
+  border-right: none;
+  margin-top: 10px;
+  border-top: none;
+  height: 50%;
 `
 
 const Footer = styled.footer `
   background-color: black;
   color: white;
+  height: 11%;
+`
+
+const Container = styled.div `
+  height: 100vh;
 `
 
 
 
 export default function MainPage() {
   return(
-    <div>
+    <Container>
         <MainHeader></MainHeader>
-        <ChartSection>
+        <Information>
           <h3>Here's a sample of what's moving fans this week.</h3>
             <ul className="Contents__List">
               <li>Songs</li>
               <li>Album</li>
               <li>Artist</li>
             </ul>
-        </ChartSection>
+          <div>
+            <img/>
+            <h3></h3>
+          </div>
+        </Information>
         <ChartSection>
           <ul>
             <li>안녕</li>
@@ -43,6 +63,6 @@ export default function MainPage() {
           </ul>
         </ChartSection>
         <Footer>ㅇㅇ</Footer>
-    </div>
+    </Container>
   )
 }
