68f02d701053f0d0afb8278ba3c939542d930e2f
limdumb <dlaruddls@nate.com>
2022-10-06 22:40:29 +0900

    Footer 분리

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Footer = styled.footer `
+  background-color: black;
+  color: white;
+  height: 11%;
+`
+export default function footer() {
+
+  return(
+    <Footer>ㅇㅇ</Footer>
+  );
+}
