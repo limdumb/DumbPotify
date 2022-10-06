ba9a403ef71a4fa02c80b4900c6d0544a2b2b6f9
limdumb <dlaruddls@nate.com>
2022-10-06 22:39:52 +0900

    Category Click Tab 분리

---
+++ src/Component/CategoryTab.jsx
@@ -0,0 +1,12 @@
+
+
+export default function CategoryTab() {
+
+  return(
+    <ul className="Contents__List">
+      <li><button>Songs</button></li>
+      <li><button>Album</button></li>
+      <li><button>Artist</button></li>
+    </ul>
+  );
+}
