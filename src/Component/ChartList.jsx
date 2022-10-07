d8487f12b2e294238a7af44f99802546e00bfc2e
limdumb <dlaruddls@nate.com>
2022-10-07 21:43:14 +0900

    List Label 추가

--- src/Component/ChartList.jsx
+++ src/Component/ChartList.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components"
-
+import CategoryTab from "./CategoryTab"
 
 const ChartListContainer = styled.div `
   background-color: white;
   border-radius: 8px;
   display: flex;
   align-items: center;
+  margin-top: 10px;
 `
 
 const ChartContainer = styled.ol `
@@ -26,16 +27,40 @@ const ChartRank = styled.div `
 export default function ChartList() {
 
   return(
-    <ChartListContainer>
-      <ChartImage src="https://upload.wikimedia.org/wikipedia/en/f/f2/BLACKPINK-_The_Album.png"/>
-        <ChartRank>
-          <span>1</span>
-          <div>new</div>
-        </ChartRank>
-          <div>
-            <p>Shut Down</p>
-            <span>Black Pink</span>
-          </div>
-    </ChartListContainer>
+    <>
+      <ChartListContainer aria-label="Tab 1">
+        <ChartImage src="https://upload.wikimedia.org/wikipedia/en/f/f2/BLACKPINK-_The_Album.png"/>
+          <ChartRank>
+            <span>1</span>
+            <div>new</div>
+          </ChartRank>
+            <div>
+              <p>Shut Down</p>
+              <span>Black Pink</span>
+            </div>
+      </ChartListContainer >
+          <ChartListContainer aria-label="Tab 2">
+          <ChartImage src="https://upload.wikimedia.org/wikipedia/en/f/f2/BLACKPINK-_The_Album.png"/>
+            <ChartRank>
+              <span>2</span>
+              <div>new</div>
+            </ChartRank>
+              <div>
+                <p>Pink Venom</p>
+                <span>Black Pink</span>
+              </div>
+        </ChartListContainer>
+            <ChartListContainer aria-label="Tab 3">
+            <ChartImage src="https://upload.wikimedia.org/wikipedia/en/f/f2/BLACKPINK-_The_Album.png"/>
+              <ChartRank>
+                <span>3</span>
+                <div>new</div>
+              </ChartRank>
+                <div>
+                  <p>Shut Down</p>
+                  <span>Ready for love</span>
+                </div>
+          </ChartListContainer>
+    </>
   );
 }
