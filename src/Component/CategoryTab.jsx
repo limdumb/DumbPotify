b5639499b78f64903bb24e0e45696d12d30cf57b
limdumb <dlaruddls@nate.com>
2022-10-07 21:44:07 +0900

    ChartList 정보 Mapping

--- src/Component/CategoryTab.jsx
+++ src/Component/CategoryTab.jsx
@@ -1,12 +1,24 @@
 
+import React, { useState } from 'react';
 
-export default function CategoryTab() {
+const CategoryTab = ({children}) => {
+  //각 탭은 정보가 여러가지 요소가 있음을 명시
+  const [activeTab, setActiveTab] = React.useState()
+
 
   return(
-    <ul className="Contents__List">
-      <li><button>Songs</button></li>
-      <li><button>Album</button></li>
-      <li><button>Artist</button></li>
-    </ul>
+    <>
+      <ul className="Contents__List">
+        {children&&children.map((tab) => <li>{tab.props.label}</li>)}
+        <li><button>Songs</button></li>
+        <li><button>Album</button></li>
+        <li><button>Artist</button></li>
+      </ul>
+        {children&&children.map((tabContent)=> (
+          <div>{tabContent.props.children}</div>
+        ))}
+    </>
   );
 }
+
+export default CategoryTab
