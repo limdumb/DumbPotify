3a66771fc40a1f10af5e79747bb5f3fbaf306f81
limdumb <dlaruddls@nate.com>
2022-10-10 23:17:41 +0900

    [Feat] Login Button 및 Logo Link 연결

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return(
       <header className="header__Container">
-        <img className="header__Logo " src={process.env.PUBLIC_URL + './logoimage/dumpotify.png'}/>
+        <Link to="/">
+         <img className="header__Logo " src={process.env.PUBLIC_URL + './logoimage/dumpotify.png'}/>
+        </Link>
+        <Link to="/login">
+          <button className="login__Button">Login</button>
+        </Link>
       </header>
   )
 }
