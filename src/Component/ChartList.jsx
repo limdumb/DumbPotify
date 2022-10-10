33080ab9a3813fb901f170671bc062ad157f5b98
limdumb <dlaruddls@nate.com>
2022-10-10 22:47:35 +0900

    [Refactor] ChartList Component 분리

--- src/Component/ChartList.jsx
+++ src/Component/ChartList.jsx
@@ -1,4 +1,18 @@
 
 export default function ChartList() {
-
+  return(
+    <ol >
+            <li className="chart__List">
+              <div className="chart__List__Content">
+                <img className="chart__List__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
+                <span>1</span>
+                <span>new</span>
+                <div>
+                  <p>Shut Down</p>
+                  <span>Black Pink</span>
+                </div>
+              </div>
+            </li>
+    </ol>
+  )
 }
