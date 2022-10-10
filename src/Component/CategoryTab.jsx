cffc214bc8748ef26f87a45f8a8f23fcedd4e095
limdumb <dlaruddls@nate.com>
2022-10-10 22:46:50 +0900

    [Refactor] CategoryTab Component 분리

--- src/Component/CategoryTab.jsx
+++ src/Component/CategoryTab.jsx
@@ -1,8 +1,30 @@
 
+export default function CategoryTab () {
 
-const CategoryTab = () => {
-
-
+  return(
+    <section className="information__Container">
+      <div className="information__Contents">
+      <h1>Black Pink</h1>
+        <ul className="information__List">
+          <li><button>Songs</button></li>
+          <li><button>Albums</button></li>
+          <li><button>Artist</button></li>
+        </ul>
+        <img className="Album__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
+      </div>
+        <div style={{ 
+          display: "flex",
+          alignItems: "center"
+      }}>
+          <span> 올해 블랙핑크는 셧다운이라는 곡으로 새로운 기록을 갱신했다. </span>
+        </div>
+        <div style={{
+          display:"flex",
+          alignItems:"flex-end"
+        }}>
+          <button>🔙</button>
+          <button>🔜</button>
+        </div>
+   </section>
+  )
 }
-
-export default CategoryTab
