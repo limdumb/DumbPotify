691ebd21ec09ab2d070bfa1401744ac8f55e52ab
limdumb <dlaruddls@nate.com>
2022-10-10 22:48:08 +0900

    [Refactor] Header Component 분리

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,4 +1,8 @@
 
 export default function Header() {
-
+  return(
+      <header className="header__Container">
+        <img className="header__Logo " src={process.env.PUBLIC_URL + './logoimage/dumpotify.png'}/>
+      </header>
+  )
 }
