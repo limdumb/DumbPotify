16fabc7293ba4bd4677d5d9350c5eaae2c649816
limdumb <dlaruddls@nate.com>
2022-10-13 09:05:56 +0900

    Tab Component 기능 구현

--- src/Component/CategoryTab.jsx
+++ src/Component/CategoryTab.jsx
@@ -1,30 +1,73 @@
+import React, { useState } from "react"
+import ChartList from "./ChartList"
 
 export default function CategoryTab () {
+  const [activate, setActivate] = useState(0)
 
-  return(
-    <section className="information__Container">
-      <div className="information__Contents">
-      <h1>Black Pink</h1>
-        <ul className="information__List">
-          <li><button>Songs</button></li>
-          <li><button>Albums</button></li>
-          <li><button>Artist</button></li>
-        </ul>
-        <img className="Album__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
-      </div>
-        <div style={{ 
-          display: "flex",
-          alignItems: "center"
+  const tabClickHandler=(index) => {
+    setActivate(index)
+  }
+
+  const tabContArr=[
+    {
+      tabTitle:(
+        <li><button onClick={() => tabClickHandler(0)}>Songs</button></li>
+      ),
+      tabContent:(
+        <ChartList/>
+      )
+    },
+
+
+    {
+      tabTitle:(
+        <li><button onClick={() => tabClickHandler(1)}>Albums</button></li>
+      ),
+      tabContent:(
+        <>
+          <ChartList/>
+          <span>안녕</span>
+        </>
+      ),
+    },
+
+
+    {
+      tabTitle:(
+          <li><button onClick={() => tabClickHandler(2)}>Artist</button></li>
+      ),
+      tabContent:(
+        <ChartList/>
+      )
+    }
+  ]
+
+    return(
+      <div style={{ display :"flex",
+                    flexDirection: "column"
       }}>
-          <span> 올해 블랙핑크는 셧다운이라는 곡으로 새로운 기록을 갱신했다. </span>
-        </div>
-        <div style={{
-          display:"flex",
-          alignItems:"flex-end"
-        }}>
-          <button>🔙</button>
-          <button>🔜</button>
-        </div>
-   </section>
-  )
+      <ul className="information__List">
+        {tabContArr.map((section, index) => {
+          return section.tabTitle
+        })}
+            </ul>
+            <div style={{
+              display:"flex",
+              alignItems:"center"
+            }}>
+            <img className="Album__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
+            <span> 올해 블랙핑크는 셧다운이라는 곡으로 새로운 기록을 갱신했다. </span>
+            </div>
+          <div>
+            {tabContArr[activate].tabContent}
+          </div>
+      </div>
+    );
 }
+
+
+{/* <ul className="information__List">
+              <li><button>Songs</button></li>
+              <li><button>Albums</button></li>
+              <li><button>Artist</button></li>
+            </ul> */}
