b83eb5687fe9541ab72c9e1cd90c3c290170d38f
limdumb <dlaruddls@nate.com>
2022-10-24 09:05:04 +0900

    Tab의 값으로 각각의 Contents 노출 구현

--- src/Component/ChartList.jsx
+++ src/Component/ChartList.jsx
@@ -1,18 +1,65 @@
+import CategoryTab from "./CategoryTab"
 
-export default function ChartList() {
+export default function ChartList(props) {
+
+  const getChartList = (tab) => {
+    if(tab === "songs") {
+      return <>
+      <ol aria-label="Content1">
+                <li className="chart__List">
+                  <div className="chart__List__Content">
+                    <img className="chart__List__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
+                    <span>1</span>
+                    <span>new</span>
+                    <div>
+                      <p>Shut Down</p>
+                      <span>Black Pink</span>
+                    </div>
+                  </div>
+                </li>
+        </ol>
+      </>
+    }
+
+    if(tab === "albums"){
+      return <>
+      <ol aria-label="Content2">
+                <li className="chart__List">
+                  <div className="chart__List__Content">
+                    <img className="chart__List__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
+                    <span>2</span>
+                    <span>new</span>
+                    <div>
+                      <p>Pink Venom</p>
+                      <span>Black Pink</span>
+                    </div>
+                  </div>
+                </li>
+        </ol>
+      </>
+    }
+
+    if(tab === "artist"){
+      return <>
+              <ol aria-label="Content3">
+                <li className="chart__List">
+                  <div className="chart__List__Content">
+                    <img className="chart__List__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
+                    <span>3</span>
+                    <span>new</span>
+                    <div>
+                      <p>Ready for love</p>
+                      <span>Black Pink</span>
+                    </div>
+                  </div>
+                </li>
+        </ol>
+      </>
+    }
+  }
   return(
-    <ol >
-            <li className="chart__List">
-              <div className="chart__List__Content">
-                <img className="chart__List__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
-                <span>1</span>
-                <span>new</span>
-                <div>
-                  <p>Shut Down</p>
-                  <span>Black Pink</span>
-                </div>
-              </div>
-            </li>
-    </ol>
+    <>
+      {getChartList(props.tab)}
+    </>
   )
 }
