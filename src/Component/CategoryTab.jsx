21be11dcff44f56ee4b4b4121538dcbd4a333a92
limdumb <dlaruddls@nate.com>
2022-10-24 09:04:17 +0900

    ChartList와 연결

--- src/Component/CategoryTab.jsx
+++ src/Component/CategoryTab.jsx
@@ -1,73 +1,75 @@
-import React, { useState } from "react"
-import ChartList from "./ChartList"
-
-export default function CategoryTab () {
-  const [activate, setActivate] = useState(0)
-
-  const tabClickHandler=(index) => {
-    setActivate(index)
-  }
-
-  const tabContArr=[
-    {
-      tabTitle:(
-        <li><button onClick={() => tabClickHandler(0)}>Songs</button></li>
-      ),
-      tabContent:(
-        <ChartList/>
-      )
-    },
+import React, { useState } from "react";
+import ChartList from "./ChartList";
+// ChartList에 props를 받아서 CategoryTab에 연결
+// 만약에 index 0번이 눌리면 Album과 Artist에 대한 정보가 display none 처리,1번이면 나머지none 2번이면 나머지 none
+// 그정보를 MainPage에 연결
 
 
-    {
-      tabTitle:(
-        <li><button onClick={() => tabClickHandler(1)}>Albums</button></li>
-      ),
-      tabContent:(
-        <>
-          <ChartList/>
-          <span>안녕</span>
-        </>
-      ),
-    },
+export default function CategoryTab({ onTabChange }) {
+  const [activate, setActivate] = useState(0);
 
-
-    {
-      tabTitle:(
-          <li><button onClick={() => tabClickHandler(2)}>Artist</button></li>
-      ),
-      tabContent:(
-        <ChartList/>
-      )
+  const tabClickHandler = (index) => {
+    setActivate(index);
+    if (onTabChange) {
+      onTabChange(index);
     }
-  ]
+  };
 
-    return(
-      <div style={{ display :"flex",
-                    flexDirection: "column"
-      }}>
+  const tabContArr = [
+    {
+      tabTitle: (
+        <li>
+          <button onClick={() => tabClickHandler(0)}>Songs</button>
+        </li>
+      )
+    },
+
+    {
+      tabTitle: (
+        <li>
+          <button onClick={() => tabClickHandler(1)}>Albums</button>
+        </li>
+      ),
+    },
+
+    {
+      tabTitle: (
+        <li>
+          <button onClick={() => tabClickHandler(2)}>Artist</button>
+        </li>
+      ),
+    },
+  ];
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <ul className="information__List">
         {tabContArr.map((section, index) => {
-          return section.tabTitle
+          return section.tabTitle;
         })}
-            </ul>
-            <div style={{
-              display:"flex",
-              alignItems:"center"
-            }}>
-            <img className="Album__Image" src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"/>
-            <span> 올해 블랙핑크는 셧다운이라는 곡으로 새로운 기록을 갱신했다. </span>
-            </div>
-          <div>
-            {tabContArr[activate].tabContent}
-          </div>
+      </ul>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <img
+          className="Album__Image"
+          src="http://www.akbobada.com/home/akbobada/archive/akbo/img/202209191012058.jpg"
+        />
+        <span>
+          올해 블랙핑크는 셧다운이라는 곡으로 새로운 기록을 갱신했다.
+        </span>
       </div>
-    );
+    </div>
+  );
 }
 
-
-{/* <ul className="information__List">
+{
+  /* <ul className="information__List">
               <li><button>Songs</button></li>
               <li><button>Albums</button></li>
               <li><button>Artist</button></li>
-            </ul> */}
+            </ul> */
+}
